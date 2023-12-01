f12c6b87ffbfc3d2437a369aa3ed87f553f20200
Святослав Минчаков <sminchakov@gmail.com>
2023-12-01 18:49:42 +0200

    init project

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Svyatoslav:99887766@cluster0.9aeffpx.mongodb.net/contacts_reader";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
