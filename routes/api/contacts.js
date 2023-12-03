a7285dd4ec293a263a7d95822f12c8e90b3f39b9
Святослав Минчаков <sminchakov@gmail.com>
2023-12-03 21:23:04 +0200

    fixed id checkout

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,19 +3,26 @@ const ctrl = require("../../controllers/contacts");
 const router = express.Router();
 const { validateBody } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
+const { isValidId } = require("../../middlewares");
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+router.get("/:contactId", isValidId, ctrl.getById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.postContact);
 
-router.delete("/:contactId", ctrl.deleteContact);
+router.delete("/:contactId", isValidId, ctrl.deleteContact);
 
-router.put("/:contactId", validateBody(schemas.putSchema), ctrl.patchContact);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.putSchema),
+  ctrl.patchContact
+);
 
 router.patch(
   "/:contactId/favorite",
+  isValidId,
   validateBody(schemas.patchSchema),
   ctrl.putContact
 );
